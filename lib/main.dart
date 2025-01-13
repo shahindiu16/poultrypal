821b3a1c60b16a24c279c2f348f65a5ec52efb9e
RomjanHossain <romjan@proton.me>
2025-01-13 17:16:12 +0600

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:poultrypal/firebase_options.dart';
 import 'package:poultrypal/splash.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
