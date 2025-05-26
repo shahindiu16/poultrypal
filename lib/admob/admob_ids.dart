a4ea4804937e57f1578b70e28dbb1245b38d8ba1
RomjanHossain <romjan@proton.me>
2025-05-26 17:58:24 +0600

    test and real ids

--- lib/admob/admob_ids.dart
+++ lib/admob/admob_ids.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class AdMobAdIds {
   // static String? get testDeviceID {
   //   if (Platform.isAndroid) {
@@ -10,27 +12,13 @@ class AdMobAdIds {
   //   return null;
   // }
 
-  static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/9214589741"; // Test ID
-      // return "YOUR_ANDROID_BANNER_AD_UNIT_ID";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/2934735716"; // Test ID
-      // return "YOUR_IOS_BANNER_AD_UNIT_ID";
-    }
-    return "ca-app-pub-3940256099942544/9214589741"; // Test ID
-  }
-
-  static String? get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712"; // Test ID
-      // return "YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4411468910"; // Test ID
-      // return "YOUR_IOS_INTERSTITIAL_AD_UNIT_ID";
-    }
-    return null;
-  }
+  // static String get testBannerAdUnitId {
+  //   return "ca-app-pub-3940256099942544/9214589741";
+  // }
+  //
+  // static String get testInterstitialAdUnitId {
+  //   return 'ca-app-pub-3940256099942544/1033173712';
+  // }
 
   static String? get rewardedAdUnitId {
     if (Platform.isAndroid) {
@@ -44,4 +32,80 @@ class AdMobAdIds {
   }
 
   // Add more ad unit types here if needed
+  static String get homeScreenbannerAdUnitId {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/8510169505"; // Test ID
+  }
+
+  static String get diseasesBannerAdUnitId1 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/5884006166"; // Test ID
+  }
+
+  static String get diseasesBannerAdUnitId2 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/7515817882"; // Test ID
+  }
+
+  static String get diseasesBannerAdUnitId3 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/6115699204"; // Test ID
+  }
+
+  static String get medicineBannerAdUnitId3 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/2710197858"; // Test ID
+  }
+
+  static String get medicineBannerAdUnitId2 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/5088949181"; // Test ID
+  }
+
+  static String get medicineBannerAdUnitId1 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/4802617533"; // Test ID
+  }
+
+  static String get infoBannerAdUnitId1 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/5876026051"; // Test ID
+  }
+
+  static String get infoBannerAdUnitId2 {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/3489535863"; // Test ID
+  }
+
+  static String get reportBannerAdUnitID {
+    if (kDebugMode) {
+      return "ca-app-pub-3940256099942544/9214589741";
+    }
+    return "ca-app-pub-8049150915836149/2462785849"; // Test ID
+  }
+
+  static String get labInterstitialAdUnitId {
+    if (kDebugMode) {
+      return 'ca-app-pub-3940256099942544/1033173712';
+    }
+    return "ca-app-pub-8049150915836149/3297964175"; // Test ID
+  }
 }
