438218b00533b0539899ee2c9763b2c8619e909f
RomjanHossain <romjan@proton.me>
2025-07-27 17:01:03 +0600

    apps qr

---
+++ lib/pages/lab/components/pdf_generate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PdfGenerate extends StatelessWidget {
+  const PdfGenerate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
